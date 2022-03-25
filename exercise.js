cbdfe1cd93815b67bc7581261a21d0a8e48ec276
Blair Bristow <blairhbristow@gmail.com>
2022-03-25 17:08:38 -0400

    Commit

--- exercise.js
+++ exercise.js
@@ -1,5 +1,14 @@
+function candySalesToObject(array) {
+    return {
+        item: array[0],
+        price: array[1],
+        quantity: array[2]
+    }
+}
+
+
 module.exports = {
     candySalesToObject,
-    salesDayToObject,
-    allSalesToArray
+    //salesDayToObject,
+    //allSalesToArray
 };
